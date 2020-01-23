7632c26cb373319a1dc6c3e7e1847d9baa2d8441
Chemambo <alexchemambo@gmail.com>
2020-01-22 23:26:04 -0800

    updated locust file with new wait_time config.

--- locustfile.py
+++ locustfile.py
@@ -1,11 +1,10 @@
-from locust import HttpLocust, TaskSet, task
+from locust import HttpLocust, TaskSet, task, between
 
 class UserBehavior(TaskSet):   
-    @task()
+    @task
     def index(self):
         self.client.get("/")
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
-    min_wait = 5000
-    max_wait = 9000
+    wait_time = between(1, 20)
